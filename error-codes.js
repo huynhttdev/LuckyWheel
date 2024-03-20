1a946ed5ce7a4e895ce5e9fb9aef73b7fc59d487
Huynh Tran <huynhtt@forlife.vn>
2024-03-20 16:34:25 +0700

    Show error code

---
+++ error-codes.js
@@ -0,0 +1,19 @@
+const errorCodes = {
+  SPIN_GAME_ONLY_PLAY_ON_VALID_TIME:
+    "Vòng quay chỉ có thể chơi trong thời gian được cấu hình trước",
+  SPIN_GAME_PLAYER_NOT_EXIST: "Thông tin người chơi không tồn tại",
+  SPIN_GAME_ONLY_PLAY_ON_EXIST_REMAIN_TURN:
+    "Không thể chơi vì người chơi đã hết lượt chơi",
+  SPIN_GAME_PLAY_NO_UNVAILABLE_PRIZES:
+    "Không thể chơi tiếp vì không còn giải thưởng",
+  SPIN_GAME_PLAY_GET_PRIZE_ERROR: "Lấy thông tin giải thưởng không thành công",
+  SPIN_GAME_PLAY_GET_CODE_ERROR:
+    "Lấy thông tin code của giải voucher không thành công",
+  SPIN_GAME_MISSION_SHARE_FACEBOOK_USED:
+    "Nhiệm vụ chia sẻ FB đã được thực hiện",
+  SPIN_GAME_NOT_EXIST_OR_DELETED: "Vòng quay không tồn tại hoặc đã bị xoá",
+  SPIN_GAME_ONLY_PLAY_ON_ACTIVE:
+    'Vòng quay chỉ có thể chơi ở trạng thái "Hoạt động"',
+  SPIN_GAME_ONLY_TESTER_PLAY_ON_TEST_MODE:
+    'Vòng quay đang ở trạng thái "Thử nghiệm", chỉ có người thử nghiệm có thể chơi',
+};
